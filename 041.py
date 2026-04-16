bc786ea7e3ed080bb0cf802af9ed4411327db8e0
Anderson Alves <anderson.filho.xap@gmail.com>
2026-04-15 21:52:31 -0300

    Faltou uns detalhes!!

--- 041.py
+++ 041.py
@@ -1,13 +1,13 @@
 ano = int(input('Qual ano você nasceu? '))
 anoatual = 2026
 idade = anoatual-ano
-if idade < 9:
+if idade <= 9:
     print('MIRIM')
-elif idade < 14:
+elif idade <= 14:
     print('INFANTIL')
-elif idade < 19:
+elif idade <= 19:
     print('Junior')
-elif idade < 25:
+elif idade <= 25:
     print('Senior')
 else:
     print('MASTER')
