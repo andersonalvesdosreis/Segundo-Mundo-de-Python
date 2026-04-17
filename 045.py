cb8c6a2018fed46c47eaec78465e9b10ffa62d46
Anderson Alves <anderson.filho.xap@gmail.com>
2026-04-17 14:54:10 -0300

    Ajustei!!

--- 045.py
+++ 045.py
@@ -1,11 +1,30 @@
 #Pedra - papel - tesoura!!
 import random
+from time import sleep
 Lista = ['Tesoura','Papel','Pedra']
 computador = random.randint(0,2)
 print(Lista)
 pergunta = int(input('Escolha um dos três, entre 0 = Tesoura, 1 = Papel , 2 = Pedra'))
 maquina = computador
 jogador = pergunta
+sleep(1)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÓ')
+if computador == 1:
+        print('Computador escolheu: Papel')
+elif computador ==2:
+        print('Computador escolheu: Pedra')
+else:
+        print('Computador escolheu: Tesoura')
+if pergunta == 1:
+        print('Você escolheu: Papel')
+elif pergunta ==2:
+        print('Você escolheu: Pedra')
+else:
+        print('Você escolheu: Tesoura')
 if maquina == 1 and jogador == 2:
         print('o Computador venceu!')
 elif maquina == 1 and jogador == 0:
@@ -20,4 +39,3 @@ elif maquina == 0 and jogador == 2:
         print('O jogador venceu!')
 else:
         print('Deu empate!')
-print(f'A escolha do computador foi:{computador}')
